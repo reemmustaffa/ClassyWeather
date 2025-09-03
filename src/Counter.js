c633d7fba823c3c5a33c03081567be812369216c
reem_mustafa <reemmustafa905@gmail.com>
2025-09-03 19:55:34 +0300

    Counter method

---
+++ src/Counter.js
@@ -0,0 +1,37 @@
+import React from "react";
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleDecrement() {
+    console.log(this);
+    this.setState((curState) => {
+      return { count: curState.count - 1 };
+    });
+  }
+  handleIncrement() {
+    console.log(this);
+    this.setState((curState) => {
+      return { count: curState.count + 1 };
+    });
+  }
+  render() {
+    const date = new Date("june 27 2027");
+    date.setDate(date.getDate() + this.state.count);
+    return (
+      <div>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>
+          {date.toDateString()} [{this.state.count}]
+        </span>
+        <button onClick={this.handleIncrement}>+</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
